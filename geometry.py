505cb1077c7da0c95530f42b0d3d5889cb78e299
Dario Vidas <dario.vidas92@gmail.com>
2016-02-12 11:19:51 +0100

    Added rotation in 3d

--- geometry.py
+++ geometry.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# TODO delete in the next commit
 def rayIntersectTriangle(ray, triangle):
     """Intersection of a ray and a triangle. Ray is a numpy of 2 points,
         triangle is a numpy of 3 points."""
@@ -44,3 +45,22 @@ def rayIntersectTriangle(ray, triangle):
         return 0, None
 
     return 1, intersection
+
+
+def rotate3d(rotAxis, rotAngle, vectors):
+    """Rotates vectors and points around the axis of rotation.
+        rotAxis     - the axis of rotation (it goes through the origin and is normalized)
+        rotAngle    - angle of rotation
+        vectors     - a list of vectors to rotate or points that need to be translated around the
+                      origin since the axis of rotation goes through origin and the rotation
+                      would not work otherwise."""
+    rotAxis = rotAxis / np.linalg.norm(rotAxis)
+    s, c = np.sin(rotAngle / 2.), np.cos(rotAngle / 2.)
+    q0, q1, q2, q3 = c, s*rotAxis[0], s*rotAxis[1], s*rotAxis[2]
+    q00, q11, q22, q33 = q0*q0, q1*q1, q2*q2, q3*q3
+    q01, q02, q03, q12, q13, q23 = q0*q1, q0*q2, q0*q3, q1*q2, q1*q3, q2*q3
+    Q = np.array(
+        [[q00 + q11 - q22 - q33,         2*(q12 - q03),         2*(q13 + q02)],
+        [         2*(q12 + q03), q00 - q11 + q22 - q33,         2*(q23 - q01)],
+        [         2*(q13 - q02),         2*(q23 + q01), q00 - q11 - q22 + q33]])
+    return map(lambda v: np.dot(Q, v), vectors)
--- raycaster.py
+++ raycaster.py
@@ -7,12 +7,32 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 import rcutils
-from geometry import rayIntersectTriangle
+from geometry import rayIntersectTriangle, rotate3d
 
 from geometry_cpp import intersect_ray_triangle, Vector, Ray, Triangle
 
-def raycaster(source, focalVec, rotation, gridSize, vertices, triangles):
+def plotP(ax, p):
+    ax.plot([p[0]], [p[1]], 'o', zs=[p[2]])
+
+def plotL(ax, a, b):
+    ax.plot([a[0], b[0]], [a[1], b[1]], '-', zs=[a[2], b[2]])
+
+def raycaster(source, focalVec, vertVec, horizVec, rotAngle, gridSize, vertices, triangles):
     pass
+    # focalVec = focalVec / np.linalg.norm(focalVec)
+    # s = np.sin(rotAngle / 2.)
+    # q0, q1, q2, q3 = np.cos(rotAngle / 2.), s*focalVec[0], s*focalVec[1], s*focalVec[2]
+    # Q = np.array(
+    #     [[q0*q0 + q1*q1 - q2*q2 - q3*q3, 2*q1*q2 - 2*q0*q3, 2*q1*q3 + 2*q0*q2],
+    #     [2*q1*q2 + 2*q0*q3, q0*q0 - q1*q1 + q2*q2 - q3*q3, 2*q2*q3 - 2*q0*q1],
+    #     [2*q1*q3 - 2*q0*q2, 2*q3*q2 + 2*q0*q1, q0*q0 - q1*q1 - q2*q2 + q3*q3]])
+    # tHoriz = horizVec #/ np.linalg.norm(horizVec)#- source
+    # tVert = vertVec #/np.linalg.norm(vertVec)#- source
+    # rHoriz = np.dot(Q, tHoriz)
+    # rVert = np.dot(Q, tVert)
+    # rHorizVec = rHoriz #+ source
+    # rVertVec = rVert #+ source
+    # return rHorizVec, rVertVec
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -23,41 +43,61 @@ if __name__ == '__main__':
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    collection = rcutils.createCollection(vertices, triangles)
+    ax.add_collection3d(Poly3DCollection(collection, facecolors='0.75', edgecolors='k'))
 
-    xsize = 32
-    ysize = 24
+    widthPxl = 32
+    heightPxl = 24
+    widthChip = 1.6
+    heightChip = 1.2
+    focalLength = 1.5
 
-    veca = np.array([1.0, 0.0, 0.0]) * 2 / xsize
-    vecb = np.array([0.0, 1.0, 0.0]) * 2 / ysize
-    offset = [-1.0, -1.0, 1.5]
-    grid = np.zeros((xsize*ysize, 3))
-    for i in xrange(xsize):
-        for j in xrange(ysize):
-            grid[i*ysize+j] = i*veca + j*vecb + offset
-    # ax.plot(grid[:,0], grid[:,1], 'r.', zs=grid[:,2])
-    
+    veca = np.array([1.0, 0.0, 0.0]) * widthChip / widthPxl
+    vecb = np.array([0.0, 1.0, 0.0]) * heightChip / heightPxl
+    focVec = np.array([0., 0., -1.]) * focalLength
     source = np.array([0., 0., 3.])
 
+    plotL(ax, source + focVec, source + focVec + veca*10)
+    plotL(ax, source + focVec, source + focVec + vecb*10)
+
+    start = time.time()
+    # veca, vecb = raycaster(source, focVec, vecb, veca, np.pi/4, None, None, None)
+    veca, vecb = rotate3d(focVec, np.pi/4, [veca, vecb])
+    print time.time() - start
+    veca = veca / np.linalg.norm(veca) * widthChip / widthPxl
+    vecb = vecb / np.linalg.norm(vecb) * heightChip / heightPxl
+    plotL(ax, source + focVec, source + focVec + veca*10)
+    plotL(ax, source + focVec, source + focVec + vecb*10)
+    offset = source + focVec - veca * widthPxl / 2. - vecb * heightPxl / 2.
+
+    plotP(ax, source)
+    plotL(ax, source, source + focVec)
+
+    start = time.time()
+    grid = np.zeros((widthPxl*heightPxl, 3))
+    for i in xrange(widthPxl):
+        for j in xrange(heightPxl):
+            grid[i*heightPxl+j] = i*veca + j*vecb + offset
+    ax.plot(grid[:,0], grid[:,1], 'r.', zs=grid[:,2])
+    print 'Grid generate', time.time() - start
+    
     start = time.time()
     for po in grid:
         # print p
         ray = np.vstack([source, po])
-        ax.plot(ray[:,0], ray[:,1], zs=ray[:,2])
+        # ax.plot(ray[:,0], ray[:,1], zs=ray[:,2])
         ray_cpp = Ray(Vector(*source), Vector(*po))
         for t in triangles:
-            
             triangle_cpp = Triangle(Vector(*vertices[t[0]]), Vector(*vertices[t[1]]), Vector(*vertices[t[2]]))
-            # print intersect_ray_triangle(Ray(Vector()))
             # f, p = rayIntersectTriangle(ray, vertices[t])
             f, p = intersect_ray_triangle(ray_cpp, triangle_cpp)
             if f == 1:
                 # ax.add_collection3d(Poly3DCollection([vertices[t]], edgecolors='k'))
-                ax.plot([source[0], p.x], [source[1], p.y], zs=[source[2], p.z])
-                ax.plot([p.x], [p.y], 'ro', zs=[p.z])
+                # ax.plot([source[0], p.x], [source[1], p.y], zs=[source[2], p.z])
+                ax.plot([p.x], [p.y], 'r.', zs=[p.z])
     
     print time.time() - start
-    collection = rcutils.createCollection(vertices, triangles)
-    ax.add_collection3d(Poly3DCollection(collection, facecolors='0.75', edgecolors='k'))
+
     ax.set_xlim3d(-2, 2)
     ax.set_ylim3d(-2, 2)
     ax.set_zlim3d(-2, 2)
